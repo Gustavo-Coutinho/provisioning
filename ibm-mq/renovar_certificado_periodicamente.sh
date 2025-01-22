9f9490404ef782e745d3b84b64988c56912decde
Gustavo Coutinho <gupoco@outlook.com>
2025-01-21 21:26:07 -0300

    Adaptar caminhos de volumes e scripts

--- ibm-mq/renovar_certificado_periodicamente.sh
+++ ibm-mq/renovar_certificado_periodicamente.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # renovar certificado certbot
-docker compose -f /home/ubuntu/mensageria2/renovar_certificado.yaml up nginx certbot -d
-docker compose -f /home/ubuntu/mensageria2/renovar_certificado.yaml exec nginx nginx -s reload
-docker compose -f ~/mensageria2/ibm-mq.yaml up -d --remove-orphans
+docker compose -f /home/ubuntu/ibm-mq/renovar_certificado.yaml up nginx certbot -d
+docker compose -f /home/ubuntu/ibm-mq/renovar_certificado.yaml exec nginx nginx -s reload
+docker compose -f ~/ibm-mq/ibm-mq.yaml up -d --remove-orphans
